1b61d85f7b6cd7f16c531609970b6b784d2599de
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-08-22 13:20:58 +0700

    chore: initialize NextRole package

---
+++ src/nextrole/__init__.py
@@ -0,0 +1,5 @@
+"""NextRole autonomous job-search agent."""
+
+from .core import JobAssessment, assess_job, build_human_decision_packet
+
+__all__ = ["JobAssessment", "assess_job", "build_human_decision_packet"]
